1b4fddb8028d5e3f77b7846e9cc71f4bf5c00d48
comphortine siwende <comphortine@kali.comsiwende@gmail.com>
2025-04-03 16:20:03 +0300

    first commit

--- deploy/deploy/urls.py
+++ deploy/deploy/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
@@ -17,8 +18,14 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    # Redirect root URL to Swagger documentation
+    path('', RedirectView.as_view(url='/swagger/', permanent=False)),
+    
+    # Admin and API endpoints
     path('admin/', admin.site.urls),
     path('api/', include('deployment.urls')),
+    
+    # Documentation endpoints
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
